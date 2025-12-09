45e03f7abbf63cc02f5526915512b4fa111a8897
EndikaPM <endika.perez.mas@gmail.com>
2025-12-09 07:40:06 +0100

    Check employee phone and PIN

--- xtendoo_app_assistance_controller/controllers/main.py
+++ xtendoo_app_assistance_controller/controllers/main.py
@@ -5,18 +5,25 @@ import json
 class XtendooAppAssistanceController(http.Controller):
     @http.route('/xtendoo/app/assistance', auth='public', type='json', methods=['POST'], csrf=False)
     def assistance(self, **kwargs):
-        print("Dentro del controller")
+        #print("Dentro del controller")
 
-       # print(f"Datos recibidos: {kwargs}")
+        print(f"Datos recibidos: {kwargs}")
 
         # Extraer los datos del JSON
-        #telefono = kwargs.get('telefono')
-        #accion = kwargs.get('accion')
-        #latitud = kwargs.get('latitud')
-        #longitud = kwargs.get('longitud')
+        telefono = kwargs.get('telefono')
+        accion = kwargs.get('accion')
+        latitud = kwargs.get('latitud')
+        longitud = kwargs.get('longitud')
+        pin = kwargs.get('pin')
 
-        #print(f"Teléfono: {telefono}, Acción: {accion}, Posición: {latitud},{longitud}")
+        print(f"Teléfono: {telefono}, Acción: {accion}, Posición: {latitud},{longitud}")
         # Aquí procesamos la petición de asistencia
         # Puedes acceder a los datos enviados en kwargs
+        #for cliente in request.env['xtendoo.cliente'].search([]):
+           # if telefono == "aqui el telefono del cliente" and pin == "aqui el pin del cliente":
+                # Lógica para manejar la petición de asistencia
+               # print("Petición de asistencia válida")
         return {'status': 'success', 'message': 'Petición de asistencia recibida', 'data': kwargs}
+
+
 
